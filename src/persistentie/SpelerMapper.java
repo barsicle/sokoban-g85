a7db8392df14b5def13b42fd9b4505446d120b72
WimStraetemansHoGent <wim.straetemans.y4649@student.hogent.be>
2020-02-18 23:21:10 +0100

    Toevoeging database link en scripts

--- src/persistentie/SpelerMapper.java
+++ src/persistentie/SpelerMapper.java
@@ -9,19 +9,19 @@ import domein.Speler;
 
 public class SpelerMapper {
 	
-	private static final String JDBC_URL = "jdbc:mysql://ID222177_dobbel.db.webhosting.be?serverTimezone=UTC&useLegacyDatetimeCode=false&user=ID222177_dobbel&password=abcd1234";
+	private static final String JDBC_URL = "jdbc:mysql://ID222177_g85.db.webhosting.be?serverTimezone=UTC&useLegacyDatetimeCode=false&user=ID222177_g85&password=fi4pinVo";
 
 	public Speler geefSpeler(String gebruikersnaam, String wachtwoord) {
         Speler speler = null;
 
         try (Connection conn = DriverManager.getConnection(JDBC_URL);
-                PreparedStatement query = conn.prepareStatement("SELECT * FROM ID222177_dobbel.speler WHERE gebruikersnaam = ?")) {
+                PreparedStatement query = conn.prepareStatement("SELECT * FROM ID222177_g85.speler WHERE gebruikersnaam = ?")) {
             query.setString(1, gebruikersnaam);
             try (ResultSet rs = query.executeQuery()) {
                 if (rs.next()) {
                 	String dbWachtwoord = rs.getString("wachtwoord");
                 	if (!dbWachtwoord.equals(wachtwoord)) {
-                		throw new IllegalArgumentException("Verkeerd wachtwoord!");
+                		throw new IllegalArgumentException("Verkeerd wachtwoord");
                 	}
                     speler = new Speler(rs.getString("gebruikersnaam"), rs.getBoolean("adminrechten"));
                 }
