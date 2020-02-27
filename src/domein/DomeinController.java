5df915b03e962858c5128c04ba51ea04c21b8585
Yarick Coppejans <yarick.coppejans@student.hogent.be>
2020-02-27 08:59:09 +0100

    Update DomeinController.java
    Javadoc comments domeincontroller toegevoegd.

--- src/domein/DomeinController.java
+++ src/domein/DomeinController.java
@@ -12,6 +12,18 @@ public class DomeinController {
 	
 	// Methods
 	// To register, the properties must meet certain conditions and the password must be confirmed.
+	
+	/**
+	 * Registreert de gebruiker als speler. Werpt een IllegalArgumentException indien
+	 *  het wachtwoord en de wachtwoordbevestiging niet overeenkomen.
+	 *
+	 * @param  naam  de naam van de gebruiker
+	 * @param  voornaam de voornaam van de gebruiker
+	 * @param  gebruikersnaam de gebruikersnaam van de gebruiker
+	 * @param  wachtwoord het wachtwoord van de gebruiker
+	 * @param  wachtwoordbevestiging de bevestiging van het wachtwoord van de gebruiker. Moet overeenkomen met het wachtwoord.
+	 */
+	
 	public void registreer(String naam, String voornaam, String gebruikersnaam, String wachtwoord, String wachtwoordBevestiging) throws IllegalArgumentException {
 		if (!wachtwoord.equals(wachtwoordBevestiging)) {
             throw new IllegalArgumentException("Wachtwoorden komen niet overeen.");
@@ -25,6 +37,13 @@ public class DomeinController {
 				
 	}
 	
+	/**
+	 * Meldt de speler aan. Werpt een RuntimeException indien
+	 *  de speler niet bestaat.
+	 *
+	 * @param  gebruikersnaam de gebruikersnaam van de speler
+	 * @param  wachtwoord het wachtwoord van de speler
+	 */
 	public void meldAan(String gebruikersnaam, String wachtwoord) throws RuntimeException {
 		if(!spelerRepository.bestaatSpeler(gebruikersnaam)) {
 			throw new RuntimeException("Speler niet gevonden");
@@ -40,15 +59,26 @@ public class DomeinController {
 		
 	}
 	
+	/**
+	 * Controleert of de speler bestaat in de repository. Werpt een RuntimeException als deze niet bestaat.
+	 * 
+	 * @param  gebruikersnaam de gebruikersnaam van de speler
+	 */
     public boolean bestaatSpeler(String gebruikersnaam) throws RuntimeException {
     	return spelerRepository.bestaatSpeler(gebruikersnaam);
     }
-	
+    
+	/**
+	 * Sluit de applicatie af.
+	 */
 	public void afsluiten() {
 		System.exit(0);
 	}
 
 	// Only return the user name and no other properties (e.g. password)
+	/**
+	 * Retourneert de speler.
+	 */
 	public Speler getSpeler() {
 		return speler;
 	}
