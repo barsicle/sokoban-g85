57c4b01e681c383a61ed389ae89718d291d9ea02
WimStraetemansHoGent <wim.straetemans.y4649@student.hogent.be>
2020-04-22 12:24:15 +0200

    Fix resources classpath

--- src/gui/ImageFactory.java
+++ src/gui/ImageFactory.java
@@ -13,13 +13,13 @@ public class ImageFactory {
 	private static final Map<SoortImage, Image> mapImage = new HashMap<>() { 
 		{ 
 			try {
-				put(WALL, new Image(new FileInputStream("resources/images/wall.png")));
-				put(VELD, new Image(new FileInputStream("resources/images/tile.png")));
-				put(DOEL, new Image(new FileInputStream("resources/images/goal.png")));
-				put(KIST, new Image(new FileInputStream("resources/images/box.png")));
-				put(MANNETJE, new Image(new FileInputStream("resources/images/dr-mario-game.png")));
-				put(LEEG, new Image(new FileInputStream("resources/images/surface.png")));
-				put(ERASER, new Image(new FileInputStream("resources/images/eraser.png")));
+				put(WALL, new Image(new FileInputStream("/resources/images/wall.png")));
+				put(VELD, new Image(new FileInputStream("/resources/images/tile.png")));
+				put(DOEL, new Image(new FileInputStream("/resources/images/goal.png")));
+				put(KIST, new Image(new FileInputStream("/resources/images/box.png")));
+				put(MANNETJE, new Image(new FileInputStream("/resources/images/dr-mario-game.png")));
+				put(LEEG, new Image(new FileInputStream("/resources/images/surface.png")));
+				put(ERASER, new Image(new FileInputStream("/resources/images/eraser.png")));
 				} catch (FileNotFoundException e) {
 					e.printStackTrace(); }
 			} 
